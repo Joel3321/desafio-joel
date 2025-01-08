5bd79d8e52e39a44cd25debe956f3ef0b08bc568
joel3321 <joelcristianodesouza@gmail.com>
2025-01-07 21:20:34 -0300

    ¨desafio

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
-let nome = "joel";
-console.log(nome);
+let nome = "**{joel}**";
+let nivelXp = 10003;
+//console.log("O herói de nome",nome,"esta");
+if (nivelXp <=1000){
+  console.log("O herói de nome",nome,"esta no nível de **{Ferro}**");
+}
+else if (nivelXp <=2000){
+  console.log("O herói de nome",nome,"esta no nível de **{Bronze}**");
+}
+else if (nivelXp <=5000){
+  console.log("O herói de nome",nome,"esta no nível de **{Prata}**");
+}
+else if (nivelXp <=7000){
+  console.log("O herói de nome",nome,"esta no nível de **{Ouro}**");
+}
+else if (nivelXp <=8000){
+  console.log("O herói de nome",nome,"esta no nível de **{Platina}**");
+}
+else if (nivelXp <=9000){
+  console.log("O herói de nome",nome,"esta no nível de **{ascendente}**");
+}
+else if (nivelXp <=10000){
+  console.log("O herói de nome",nome,"esta no nível de **{Imortal}**");
+}
+else {
+  console.log("O herói de nome",nome,"esta no nível de **{Radiante}**");
+}
